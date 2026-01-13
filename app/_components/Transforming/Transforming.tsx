7e9e986522634ea97edae5b297416daea019b1e8
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-01-13 18:13:32 +0530

    fix

--- app/_components/Transforming/Transforming.tsx
+++ app/_components/Transforming/Transforming.tsx
@@ -8,8 +8,8 @@ export const Transforming = () => {
             <h1 className="text-3xl font-bold text-[#0b477f] px-5 mb-5">
                 Where Our Teachers Are Transforming Classrooms
             </h1>
-            <div className="p-10">
-            <Marquee logos={logos} speed={70} pauseOnHover={false}/>
+            <div className="w-full overflow-hidden contain-layout_paint transform-[translateZ(0)]">
+                <Marquee logos={logos} speed={70} pauseOnHover={false} />
             </div>
         </div>
     );
